b03c6ba6a59438b275cfd313eb58a60a08f1aa5b
WillyBNI <shahrad.aryanmehr@gmail.com>
2026-06-22 18:09:16 +0330

    implimented close button functionality

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,29 @@ import Window from "./components/window";
 import { useState } from 'react'
 
 function App() {
-const [windows, setWindows] = useState(defaultWindows);
+const [windows, setWindows] = useState(defaultWindows)
+const closeWindow = (id: string) => {
+  setWindows(prev =>
+    prev.map(window =>
+      window.id === id
+        ? { ...window, isOpen: false }
+        : window
+    )
+  );
+};
 
   return (
     <body style={{ backgroundColor: '#008080' }}>
-          {windows.map((window) => (
-            <Window
-              key={window.id}
-              window={window}
-            >
-              <p>huh huh huh woah</p>
-            </Window>
-          ))}
+      {windows
+        .filter(window => window.isOpen)
+        .map(window => (
+        <Window
+          key={window.id}
+          window={window}
+          onClose={() => closeWindow(window.id)}>
+            <p>some stuff</p>
+        </Window>
+      ))}
     </body>
   )
 }
